ed3ac1b10feb8e88b56cdbfd4b4ac34c53770597
Sujit-Badgujar <112996880+Sujit-Badgujar@users.noreply.github.com>
2023-03-08 16:19:34 +0530

    Add files via upload
    This is a Cart Entity file of Easy-Pharma web app.

---
+++ EasyPharmaBackend/com.easypharma.entities/Cart.java
@@ -0,0 +1,96 @@
+package com.easypharma.entities;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cartId;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+//, referencedColumnName = "productId"
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @JsonIgnore
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+
+    private int quantity;
+
+    public Cart() {
+    }
+
+    public Cart(Product product, int quantity, User user){
+        this.user = user;
+        this.product = product;
+        this.quantity = quantity;
+        this.createdDate = new Date();
+    }
+
+ 
+    public Integer getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(Integer cartId) {
+		this.cartId = cartId;
+	}
+
+	public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+	@Override
+	public String toString() {
+		return "Cart [cartId=" + cartId + ", product=" + product + ", user=" + user + ", quantity=" + quantity + "]";
+	}
+    
+}
