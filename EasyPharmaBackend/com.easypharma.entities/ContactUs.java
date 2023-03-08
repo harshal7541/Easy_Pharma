9126812af51f8179a9dff802637157bedba11b25
SaketBhavsar <121427790+SaketBhavsar@users.noreply.github.com>
2023-03-08 16:18:03 +0530

    Add files via upload

---
+++ EasyPharmaBackend/com.easypharma.entities/ContactUs.java
@@ -0,0 +1,78 @@
+package com.easypharma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contactus")
+public class ContactUs {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int id;
+	private String name;
+	private String email;
+	private String subject;
+	private String message;
+	
+	public ContactUs() {
+	}
+
+	public ContactUs(int id, String name, String email, String subject, String message) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.subject = subject;
+		this.message = message;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "ContactUs [id=" + id + ", name=" + name + ", email=" + email + ", subject=" + subject
+				+ ", message=" + message + "]";
+	}
+}
+
