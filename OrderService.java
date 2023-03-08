88e831623997f80be77f43ae13d7d3862c6f62ac
ShekharPadam <121109760+ShekharPadam@users.noreply.github.com>
2023-03-08 15:52:52 +0530

    Add files via upload

---
+++ OrderService.java
@@ -0,0 +1,14 @@
+package com.easypharma.services;
+
+import java.util.List;
+
+import com.easypharma.dtos.PlaceOrderDTO;
+import com.easypharma.entities.Order;
+import com.easypharma.entities.User;
+
+public interface OrderService {
+	
+	//public Order saveOrder(PlaceOrderDTO orderDto, User user); my code
+	
+
+}
